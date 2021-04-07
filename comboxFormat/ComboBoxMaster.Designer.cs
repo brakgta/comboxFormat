d44d6c0b1e5f6b3fb4d663214e71eddf1146b67a
brakgta santos <brakgta123@gmail.com>
2021-04-07 18:22:49 +0100

    Add .gitignore and .gitattributes.

---
+++ comboxFormat/ComboBoxMaster.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace comboxFormat
+{
+    partial class ComboBoxMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            this.comboBox1page1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // comboBox1page1
+            // 
+            this.comboBox1page1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1page1.FormattingEnabled = true;
+            this.comboBox1page1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1page1.Name = "comboBox1page1";
+            this.comboBox1page1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1page1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1page1.TabIndex = 0;
+            this.comboBox1page1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 20);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(162, 3);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Visible = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick_1);
+            // 
+            // ComboBoxMaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1page1);
+            this.Name = "ComboBoxMaster";
+            this.Size = new System.Drawing.Size(163, 23);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox comboBox1page1;
+        public System.Windows.Forms.ListView listView1;
+    }
+}
